e6aec89529bb6b9d132127cf08997320ba798f04
Kevin Watters <kevinwatters@gmail.com>
2018-05-27 06:56:10 -0400

    show changes in balance

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 extern crate web3;
-//extern crate tokio_core;
-
-//use tokio_core::reactor;
 use web3::futures::Future;
-//use web3::types::BlockNumber;
 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::{thread, time};
+
+use web3::types::{H160, U256};
+
+
+pub struct AddressInfo {
+    pub address:H160,
+    pub balance:U256,
+}
 
 fn main() {
     let ipc_socket = "/Users/kevin/Library/Application Support/io.parity.ethereum/jsonrpc.ipc";
     let (_el, ipc) = web3::transports::Ipc::new(ipc_socket).unwrap();
-
     let web3 = web3::Web3::new(ipc);
 
-    /*
-    let block = web3.eth().block_with_txs(
-        BlockNumber::Latest.into(),
-    ).wait().unwrap();
-    */
+    let mut addrs = vec![];
 
     for line_result in BufReader::new(File::open("addresses.txt").unwrap()).lines() {
         let line = line_result.unwrap();
@@ -27,7 +27,20 @@ fn main() {
         }
 
         let address_str = line.clone();
-        let balance = web3.eth().balance(line.parse().unwrap(), None).wait().unwrap();
-        println!("address {:?} has: {:?} ETH", address_str, balance);
+        let addr:H160 = line.parse().unwrap();
+        addrs.push(AddressInfo { address: addr, balance: 666.into() });
+    }
+
+    loop {
+        for ref mut addr_entry in &mut addrs {
+            let addr = addr_entry.address;
+            let balance = web3.eth().balance(addr, None).wait().unwrap();
+            if balance != addr_entry.balance {
+                println!("address {:?} has: {:?} ETH", addr, balance);
+                addr_entry.balance = balance;
+            }
+        }
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
